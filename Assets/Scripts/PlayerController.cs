b236a44a5d2a3ff9da57df885c9143cae74125fd
Jooyink <56498594+Jooyink@users.noreply.github.com>
2025-09-08 16:07:25 -0600

    Dash y balas personaje

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public float dashSpeed = 10f;
+    public float dashDuration = 0.2f; // cuanto dura el dash
+    private bool isDashing = false;
+
     public Animator animator;
 
     void Update()
     {
-        float move = Input.GetAxis("Horizontal"); // teclas A-D o flechas
-
-        animator.SetFloat("movement", move);
-
-        if (move > 0)
+        if (!isDashing) // solo se mueve normal si no está en dash
         {
+            float move = Input.GetAxis("Horizontal");
 
-            transform.localScale = new Vector3(-1, 1, 1);
-         }
-           if (move < 0)
+            animator.SetFloat("movement", Mathf.Abs(move));
+
+            if (move > 0)
+                transform.localScale = new Vector3(-1, 1, 1);
+
+            if (move < 0)
+                transform.localScale = new Vector3(1, 1, 1);
+
+            transform.Translate(Vector2.right * move * speed * Time.deltaTime);
+
+            // Dash con tecla E
+            if (Input.GetKeyDown(KeyCode.E) && move != 0)
+            {
+                StartCoroutine(Dash(move));
+            }
+        }
+    }
+
+    IEnumerator Dash(float direction)
+    {
+        isDashing = true;
+
+        float time = 0;
+        while (time < dashDuration)
         {
+            transform.Translate(Vector2.right * direction * dashSpeed * Time.deltaTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
 
-            transform.localScale = new Vector3(1, 1, 1);
-         }
-
-        transform.Translate(Vector2.right * move * speed * Time.deltaTime);
-
-  
+        isDashing = false;
     }
 }
